ce4007eeb2e8618754524bdd995f1aa03c3f628f
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-11-21 10:41:18 +0100

    Updates

---
+++ precache-manifest.ef21db31b8f2e68600e00214b9f8846e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdcf71c027ae1e69c8f99514a108cc1",
+    "url": "/nav-menu-react/index.html"
+  },
+  {
+    "revision": "0cc47e0352164c8a399e",
+    "url": "/nav-menu-react/static/css/main.9709a0c9.chunk.css"
+  },
+  {
+    "revision": "61c573bd3e3fb0b0928c",
+    "url": "/nav-menu-react/static/js/2.8820b8a7.chunk.js"
+  },
+  {
+    "revision": "0cc47e0352164c8a399e",
+    "url": "/nav-menu-react/static/js/main.d8a8238e.chunk.js"
+  },
+  {
+    "revision": "993c0c2d594ed18c565b",
+    "url": "/nav-menu-react/static/js/runtime-main.d4508186.js"
+  }
+]);
